16ccfbb132391a2f3541fc28854fb248a64b6b30
rich wilk <richardwilkinson@live.com>
2021-01-21 19:11:12 -0500

    Update html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -53,4 +53,16 @@ module.exports = function(app) {
 
     res.render("add-topping", hbsObject);
   });
+  app.get("/index", async (req, res) => {
+    const orders = await db.Order.findAll({});
+
+    const orderToppings = await db.OrderToppings.findAll({});
+
+    const hbsObject = {
+      orders,
+      orderToppings
+    };
+
+    res.render("index", hbsObject);
+  });
 };
