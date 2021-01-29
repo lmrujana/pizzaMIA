6605196f073574904c8d2b379f98a38f3160c023
rich wilk <richardwilkinson@live.com>
2021-01-29 14:37:20 -0500

    added employee role to signup process

--- models/user.js
+++ models/user.js
@@ -13,6 +13,10 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       unique: true,
       allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
--- public/js/signup.js
+++ public/js/signup.js
@@ -3,30 +3,37 @@ $(document).ready(() => {
   const signUpForm = $("form.signup");
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
+  const roleInput = $("input#role-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
     event.preventDefault();
     const userData = {
       username: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
+      role: roleInput
+        .val()
+        .trim()
+        .toLowerCase()
     };
 
     if (!userData.username || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.username, userData.password);
+    signUpUser(userData.username, userData.password, userData.role);
     emailInput.val("");
     passwordInput.val("");
+    roleInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(username, password) {
+  function signUpUser(username, password, role) {
     $.post("/api/signup", {
       username: username,
-      password: password
+      password: password,
+      role: role
     })
       .then(() => {
         window.location.replace("/index");
