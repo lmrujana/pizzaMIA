5bc62ae735254f0517ca274951b50a131cc30135
Joivre Charles <astemcode@gmail.com>
2021-03-06 13:59:01 -0500

    test

--- models/index.js
+++ models/index.js
@@ -11,6 +11,7 @@ var helpers = require('handlebars-helpers')();
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  
   // var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
